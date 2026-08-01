82e45c6591f8d1547aaa39f5e5dd827289d2742f
Irin Akter <irinakter2926@gmail.com>
2026-08-01 12:53:43 +0600

    fix import casing for Components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from '../Components/Banner';
 import Flashsales from '../Components/Flashsales';
-import Categories from '../components/Categories'
+import Categories from '../Components/Categories'
 // import CategorySection from '../Components/CategorySection';
 
 const Home = () => {

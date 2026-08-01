615b7720822b5ae64804b963cc1259cc762169d4
Irin Akter <irinakter2926@gmail.com>
2026-08-01 13:38:53 +0600

    fix commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Banner from '../Components/Banner';
 import Flashsales from '../Components/Flashsales';
 import Categories from '../Components/Categories'
-// import CategorySection from '../Components/CategorySection';
+
 
 const Home = () => {
   return (
